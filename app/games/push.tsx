a74010033120e5d6e826ea02ed00612284ac4d9f
Liqiuzhi Yang <lee4works@gmail.com>
2023-11-12 19:08:56 -0800

    change for style

--- app/games/push.tsx
+++ app/games/push.tsx
@@ -15,41 +15,37 @@ const Push = ({ username }) => {
             body: JSON.stringify({})
         })
     }
-    const buttonStyle = "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium justify-end"
+    const buttonStyle = "text-white hover:bg-red-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium ml-auto transition ease-in-out delay-100 hover:scale-105"
 
     return (
         <div>
             <div className="min-h-full">
                 <nav className="bg-gradient-to-b from-black from-80%">
                     <div className="px-w-full sm:px-12 lg:px-8">
-                        <div className="flex h-20 items-center justify-between">
-                            <div className="">
-                                <div className="flex items-baseline space-x-4">
-                                    <a onClick={() => { router.push('/games/create/') }} className="text-white hover:bg-red-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium transition ease-in-out delay-100 hover:scale-105">Create New</a>
+                        <div className="flex h-10 items-center justify-between">
+                                <div className="flex items-baseline space-x-4 ">
+                                    <a onClick={() => { router.push('/games/create/') }}
+                                        className={buttonStyle}>
+                                        Create New</a>
                                 </div>
-                                <p
-                                        className="text-white hover:bg-red-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium ml-auto transition ease-in-out delay-100 hover:scale-105"
+                                <div className="flex justify-items-end">
+                                    <p
+                                        className={buttonStyle}
                                     >Welcome Back! {username}</p>
                                     <button
-                                        className="text-white hover:bg-red-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium ml-auto transition ease-in-out delay-100 hover:scale-105"
+                                        className={buttonStyle}
                                         onClick={() => {
                                             router.push('/profile')
                                         }
                                         }>Edit Profile</button>
-                                    <div>
-                            </div>
-                            <div className="">
-                                <div className="flex items-baseline space-x-4">
                                     <button
-                                        className="text-white hover:bg-red-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium ml-auto transition ease-in-out delay-100 hover:scale-105"
+                                        className={buttonStyle}
                                         onClick={() => {
-                                            signOut({callbackUrl:'/'})
+                                            signOut({ callbackUrl: '/' })
                                             fetchresdata()
                                         }}>Sign Out</button>
-                                </div>
+                        </div>
                             </div>
-                        </div>
-                        </div>
                     </div>
                 </nav>
             </div>
