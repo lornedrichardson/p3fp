672b539220884989b15864508f5df7b5503080e7
Alex <halos959@gmail.com>
2023-11-12 18:07:58 -0800

    Signup Page styled Mobile

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -70,7 +70,7 @@ export default function Page() {
                     <div>
                         <label className="block text-sm font-medium leading-6 text-gray-200">Username</label>
                         <input 
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-lg sm:leading-6 indent-2"
+                        className="block w-40 md:w-80 lg:w-96 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-lg sm:leading-6 indent-2 mx-auto"
                         type="text" 
                         defaultValue={username}
                         onChange={(e) => setUsername(e.target.value)} />
@@ -78,23 +78,23 @@ export default function Page() {
                     <div>
                         <label className="block text-sm font-medium leading-6 text-gray-200">Password</label>
                         <input 
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 indent-2"
+                        className="block w-40 md:w-80 lg:w-96 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 indent-2 mx-auto"
                         type="password" 
                         onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div>
-                        <label className="block text-sm font-medium leading-6 text-gray-400">Email</label>
+                        <label className="block text-sm font-medium leading-6 text-gray-200">Email</label>
                         <input 
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-40 md:w-80 lg:w-96 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 indent-2 mx-auto"
                         type="email" 
                         defaultValue={email}
                         onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <button 
-                    className="flex w-full justify-center rounded-md bg-gradient-to-r from-red-600 via-purple-900 to-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gradient-to-l from-red-600 via-purple-900 to-blue-700 transition ease-in-out delay-100 hover:scale-105"
+                    className="flex w-40 md:w-80 lg:w-96 justify-center rounded-md bg-gradient-to-r from-red-600 via-purple-900 to-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gradient-to-l from-red-600 via-purple-900 to-blue-700 transition ease-in-out delay-100 hover:scale-105 mx-auto"
                     type='submit'>Sign Up</button>
                     <button type="button"  
-                    className="flex w-full justify-center rounded-md bg-gradient-to-r from-red-600 via-purple-900 to-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gradient-to-l from-red-600 via-purple-900 to-blue-700 transition ease-in-out delay-100 hover:scale-105"
+                    className="flex w-40 md:w-80 lg:w-96 justify-center rounded-md bg-gradient-to-r from-red-600 via-purple-900 to-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gradient-to-l from-red-600 via-purple-900 to-blue-700 transition ease-in-out delay-100 hover:scale-105 mx-auto"
                     onClick={() => {
                         signOut({ callbackUrl: '/' })
                         }}>Login page</button>
